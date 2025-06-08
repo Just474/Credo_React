f42165a607950a86fc8f4d8d064c2f8bf34bbc1c
Just474 <jastjast4739200610@gmail.com>
2025-06-08 13:27:08 +0700

    add all Routes and all pages

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+            <Header/>
+                <Routes>
+                    <Route path="*" element={<Page404/>}></Route>
+                    <Route path="/" element={<Main/>} />
+                    <Route path='/consultation' element={<Consultation/>}/>
+                    <Route path='/catalog' element={<Catalog/>}/>
+                    <Route path='/news' element={<News/>}/>
+                    <Route path='/news/1' element={<SingleNews/>}/>
+                    <Route path='/cart' element={<Cart/>}/>
+                </Routes>
+            <Footer/>
+        </BrowserRouter>
   );
 }
 
